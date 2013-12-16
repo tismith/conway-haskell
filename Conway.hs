db5de26fd75e877bb066d3e60d176a71aa76e2eb
Toby Smith <toby@tismith.id.au>
2013-12-16 11:31:21 +1000

    Remove tests from Conway.hs
    And add some HUnit tests on the conway game of life algo.

--- Conway.hs
+++ Conway.hs
@@ -1,7 +1,6 @@
 module Conway where
 import Data.List (nub, intersect)
 import Control.Monad (forM_)
-import Test.QuickCheck
 
 type Cell = (Int, Int)
 
@@ -10,11 +9,8 @@ startingCells :: [Cell]
 startingCells = [(1,0), (2,1), (0,2), (1,2), (2,2), --glider
 	(5,5),(5,4),(5,6)]
 
--- add cells who have 3 alive neighBours
 nextGeneration :: [Cell] -> [Cell]
 nextGeneration cs = nub $ concat $ map (newAlive cs) cs
-prop_nextGeneration_empty cs =
-	(null cs) ==>  null (nextGeneration cs)
 
 newAlive :: [Cell] -> Cell -> [Cell]
 newAlive cs c = filter (isAlive cs) $ c:(neighBours c)
@@ -25,7 +21,6 @@ neighBours (x,y)
         ax <- [x-1, x, x+1], 
         ay <- [y-1, y, y+1],
         ax /= x || ay /= y]
-prop_neighBours_length c = length (neighBours c) == 8
 
 -- takes a gameboard and cell, and returns if it's alive next gen 
 -- or not
@@ -38,8 +33,6 @@ isAlive cs c
     | otherwise = False
     where
         aliveCount = length $ liveNeighbours cs c
-prop_isAlive_empty cs c  =
-	(null cs) ==>  (isAlive cs c) == False
 
 -- takes a list of live cells, and a coord, and 
 -- returns a list of the neighbours that are currently alive
@@ -55,8 +48,6 @@ maxY = maximum . (map (snd))
 maxX = maximum . (map (fst))
 minY = minimum . (map (snd))
 minX = minimum . (map (fst))
-prop_max_min_X cs = (not (null cs)) ==> (maxX cs) >= (minX cs)
-prop_max_min_Y cs = (not (null cs)) ==> (maxY cs) >= (minY cs)
 
 printCells :: [Cell] -> IO ()
 printCells [] = return ()
--- Test.hs
+++ Test.hs
@@ -1,22 +1,57 @@
 module Main (main) where
 
 import Test.Framework
+import Test.QuickCheck
+import Test.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.Framework.Providers.HUnit
 
+import Data.List (nub)
 import Conway
 
 main = defaultMain tests
+
+prop_nextGeneration_empty cs =
+	(null cs) ==>  null (nextGeneration cs)
+
+prop_neighBours_length c = length (neighBours c) == 8
+prop_neighBours_bounds_maxx c = maxX (neighBours c) == (fst c) + 1
+prop_neighBours_bounds_minx c = minX (neighBours c) == (fst c) - 1
+prop_neighBours_bounds_maxy c = maxY (neighBours c) == (snd c) + 1
+prop_neighBours_bounds_miny c = minY (neighBours c) == (snd c) - 1
+prop_neighBours_unique c = 
+	length (nub (neighBours c)) == length (neighBours c)
+
+prop_isAlive_empty cs c  =
+	(null cs) ==>  (isAlive cs c) == False
+
+prop_max_min_X cs = (not (null cs)) ==> (maxX cs) >= (minX cs)
+prop_max_min_Y cs = (not (null cs)) ==> (maxY cs) >= (minY cs)
+
+horizontalBar = [(0,1), (1,1), (2,1)]
+verticalBar = [(1,0), (1,1), (1,2)]
+test_nextGeneration_hbar = (nextGeneration horizontalBar) @?= verticalBar 
+test_nextGeneration_vbar = (nextGeneration verticalBar) @?= horizontalBar 
 
 tests = 
 	[
 		testGroup "Basic properties"
 		[ 
 			testProperty "neighBours length " prop_neighBours_length,
+			testProperty "neighBours bounds min x" prop_neighBours_bounds_minx,
+			testProperty "neighBours bounds max x" prop_neighBours_bounds_maxx,
+			testProperty "neighBours bounds min y" prop_neighBours_bounds_miny,
+			testProperty "neighBours bounds max y" prop_neighBours_bounds_maxy,
+			testProperty "neighBours unique" prop_neighBours_unique,
 			testProperty "isAlive empty" prop_isAlive_empty,
 			testProperty "nextGeneration empty" prop_nextGeneration_empty,
 			testProperty "max X >= min X" prop_max_min_X,
 			testProperty "max Y >= min Y" prop_max_min_Y
+		],
+		testGroup "Functional tests"
+		[
+			testCase "nextGeneration v-bar" test_nextGeneration_vbar,
+			testCase "nextGeneration h-bar" test_nextGeneration_hbar
 		]
 	]
 	
